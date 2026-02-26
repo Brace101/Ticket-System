d6bb05b4d3dc7315fba7982dedea38001b4255db
Adeyemi Olalekan <deyemibowen@gmail.com>
2026-02-26 14:53:27 +0100

    first commit

---
+++ vowel.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Vowel{
+public static void main (String []args){
+Scanner input = new Scanner(System.in);
+
+System.out.print("Enter your name");
+string name=input.nextString;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+}
